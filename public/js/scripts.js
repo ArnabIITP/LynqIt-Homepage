c08841a600b0b1ed93ad964a7012decf4dc0b6a6
ArnabIITP <arnabdasdgp020@gmail.com>
2025-04-12 16:48:41 +0530

    Delete public directory

--- public/js/scripts.js
+++
@@ -1,7 +0,0 @@
-// Custom JavaScript - Form handling can be added here
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    alert(`Thanks! We'll notify you at ${email} when we launch.`);
-    document.getElementById('contactForm').reset();
-});
